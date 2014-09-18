2c096aea57f5e94571a1a1ebc49bbdcd262421db
amzotti <azotti1337@gmail.com>
2014-09-18 11:57:30 -0700

    Insert headings into readme

--- readme.mk
+++ readme.mk
@@ -1,1 +1,17 @@
-This is a one page HTML/CSS sample portfolio site with links to all my potential future projects. I have created it in alignment to the deadlines established in CSCI E-15
+
+<h1><a name="user-content-xkcd-password-generator" class="anchor" href="#xkcd-password-generator" rel="noreferrer"><span class="octicon octicon-link"></span></a> "P1: Dynamic Web Apps portfolio"</h1>
+
+<h2>Description</h2>
+This is a one page HTML/CSS sample portfolio site with links to all my potential future projects. I have created it in alignment to the deadlines established in CSCI E-15. My intention for this project is to brush up on my HTML/CSS skills and learn the needed workflow. There should be a repository for this project on my local machine, on my remote repo (github), and on my deployment server (DigitalOcean).
+
+<h2>Live URL</h2>
+TBA
+
+<h2>Demo</h2>
+Not applicable
+
+<h2>Details for teaching team</h2>
+Not applicable
+
+<h2>Outside code</h2>
+Not applicable
